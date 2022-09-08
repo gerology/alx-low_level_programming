18588e4705b717217d9387b5d6c3f365c06143f2
gerology <gerology007@gmail.com>
2022-09-08 16:04:40 -0700

    hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point for code
+ *
+ * description: "Programming is like building a multilingual puzzle
+ *
+ * return - always 0
+ */
+int main(void)
+{ 
+    print("Programming is like building a multilingual puzzle\n")
+	return(0);
+}
+
