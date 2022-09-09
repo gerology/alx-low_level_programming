fdf911e0f982e4be6750752ec252bccce7266dfe
gerology <gerology007@gmail.com>
2022-09-08 21:32:44 -0700

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- * main - Entry point for code
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
- * description: "Programming is like building a multilingual puzzle
- *
- * return - always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
