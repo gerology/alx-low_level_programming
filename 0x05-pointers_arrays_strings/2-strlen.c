c53a5776b7288b31d760c11ae3bc764e6e003eb4
gerology <gerology007@gmail.com>
2022-09-19 10:11:08 -0700

    return string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,18 +2,23 @@
 
 /**
  * _strlen - returns the length of string
- * Return - length of string
+ *
+ * @s: is a string
+ *
+ *
+ * Return: length of string
+ *
  */
 
 int _strlen(char *s)
 {
-	int len=0
+	int len = 0;
 
-	while(s[len] != \'0');
+	while (s[len] != \'0')
 	{
 		len++;
 	}
 
-	return (len);	
+	return (len);
 
 }
