09b2d1a1826b3c819be76cde1489d8b64921bf7b
gerology <gerology007@gmail.com>
2022-09-19 10:40:23 -0700

    print string and a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ *
+ * @str: string
+ *
+ * return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar ('\n');
+
+	return (void);
+}
