271dd0deff61ee4883b795df523df89a0878dc10
gerology <gerology007@gmail.com>
2022-09-19 17:14:03 -0700

    print half of the string

---
+++ 0x05-pointers_arrays_strings/main_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void puts_half(char *);
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+	str = " ";
+
+	puts_half(str);
+
+	return (0);
+}
