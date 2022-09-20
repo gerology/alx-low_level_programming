5fbadadaf2d314bd41339b81e3f930edf08eb550
gerology <gerology007@gmail.com>
2022-09-19 19:06:52 -0700

    print half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,33 +1,24 @@
 #include "main.h"
 
 /**
- *puts_half - prints half of a string
- *
- * @str: string
+ * puts_half - prints the second half of a string
+ * @str: string to print
  *
  * Return: void
  */
-
 void puts_half(char *str)
 {
+	int j, i = 0;
 
-	int i, j, max;
-
-	i = 0;
-
-	while (str[i] != '\0')
-	{
+	while (*(str + i))
 		i++;
-	}
-	max = i;
-	j = max / 2;
-
-	while (j <= max)
+	j = i / 2;
+	if (i % 2)
+		j += 1;
+	while (j < i)
 	{
-		_putchar(str[j]);
+		_putchar(*(str + j));
 		j++;
 	}
-
 	_putchar('\n');
 }
-
