407315fe034012739b99b5893f536f028bebfde2
gerology <gerology007@gmail.com>
2022-09-28 15:01:25 -0700

    print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,12 +2,10 @@
 
 /**
  * _puts_recursion - prints a string followed by a new line
- *
- * @s: string to be printed
+ * @s: string to print
  *
  * Return: void
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
