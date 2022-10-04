d9c5b960c2c5b6c31dc59458686ddeeaabe0f74b
gerology <gerology007@gmail.com>
2022-10-04 02:09:38 -0700

    create array and initialize

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,19 @@
 
 char *create_array(unsigned int size, char c)
 {
-	int *v;
 	unsigned int i;
+	char *v;
 
 	if (size == 0)
 		return (NULL);
-	v = malloc(sizeof(char) * size);
+	v = malloc(size * sizeof(char));
 	if (v == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
-	v[0] = c;
+		v[i] = c;
 	}
 	return (v);
-
 }
