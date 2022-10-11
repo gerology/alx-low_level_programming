5ce52a8fc5ea4e4ae58cd652f5772cf4ff6935e1
gerology <gerology007@gmail.com>
2022-10-11 13:14:53 -0700

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,27 +9,37 @@
  *
  * Return: pointer if success, and NULL if fail
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned  int n, o, i;
 	dog_t *puppy;
 
 	if (name == NULL || owner == NULL)
+
 		return (NULL);
+
 	puppy = malloc(sizeof(dog_t));
+
 	if (puppy == NULL)
+		
 		return (NULL);
+
 	for (n = 0; name[n]; n++)
 		;
 	n++;
+	
 	puppy->name = malloc(sizeof(char) * n);
+
 	if (puppy->name == NULL)
 	{
 		free(puppy);
 		return (NULL);
 	}
+
 	for (i = 0; i < n; i++)
 		puppy->name[i] = name[i];
+	
 	puppy->age = age;
 
 	for (o = 0; owner[o]; o++)
@@ -44,7 +54,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(puppy);
 		return (NULL);
 	}
+	
 	for (i = 0; i < o; i++)
+
 		puppy->owner[i] = owner[i];
+
 	return (puppy);
 }
