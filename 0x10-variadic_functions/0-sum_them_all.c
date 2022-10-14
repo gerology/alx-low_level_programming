f03c101d56a34569fe1b6bb62fda132630d6f0a0
gerology <gerology007@gmail.com>
2022-10-13 20:33:02 -0700

    sum all parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,32 +6,19 @@
  * sum_them_all - returns the sum of parameters
  *
  * @n: number of arguments
- * @sum: some of all arguments
- * Return: sum if successful, 0 if failed
+ * @total: some of all arguments
+ * Return: total if successful, 0 if failed
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	int total = 0;
+	unsigned int i;
+	va_list ags;
 
-	va_list arg;
-
-	va_start(arg, n);
-
-
-	if (n != 0)
-	{
-		unsigned int i;
-		int go;
-
-		for (i = 0; i < n; i++)
-		{
-			go = va_arg(arg, int);
-
-			total += go;
-		}
-
-	}
-	va_end(arg);
+	va_start(ags, n);
+	for (i = 0; i < n; i++)
+		total += va_arg(ags, int);
+	va_end(ags);
 	return (total);
 }
