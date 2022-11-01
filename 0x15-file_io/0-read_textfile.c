a763804c4b85e823cf64c27c898af529e9b7c0f8
gerology <gerology007@gmail.com>
2022-11-01 04:08:45 -0700

    create a file 2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
  * @filename: pointer to file to be read
  * @letters: number of letters to be read and printed
  *
-Return: number of letters if successful, 0 if failed
+ * Return: number of letters if successful, 0 if failed
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
