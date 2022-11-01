cc90fbbfaa9ea76d9f4ca09e8c20d3db97cde9c5
gerology <gerology007@gmail.com>
2022-10-31 17:54:16 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; i < text_content[len]; i++)
 		len++;
 
-	if (filename ==NULL)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-
+i
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	cr = write(fd, text_content, len);
 
