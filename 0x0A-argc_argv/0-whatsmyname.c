77c35670a74db86574d49175d8c95621236049ed
gerology <gerology007@gmail.com>
2023-04-10 17:43:04 +0000

    print name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of this program
+ *
+ * @argc - number of arguments passed
+ * @argv - an array of pointers to arguments
+ * 
+ * Return: Always 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+    printf("%s\n", argv[0]);
+
+    return (0);
+}
