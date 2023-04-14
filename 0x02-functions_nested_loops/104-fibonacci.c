099776b04a693a470ed2c6cc60765767f2f90511
gerology <gerology007@gmail.com>
2023-04-14 05:30:26 +0000

    first 98 fibanacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int c = 2, i;
+	unsigned long fib[98];
+	fib[0] = 0;
+	fib[1] = 1;
+
+	while (c <= 92)
+	{
+		fib[c] = fib[c - 1] + fib[c - 2];
+		c++;
+	}
+
+	for (i = 0; i < 98; i++)
+	{
+		printf("%lu, ", fib[i]);
+	}
+
+	return (0);
+
+}
