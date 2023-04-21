a8efd5bff1e3222327c82df876dea8ee6767a234
gerology <gerology007@gmail.com>
2023-04-21 15:07:42 +0000

    print all elements

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * size_t print_dlistint - print all elements of dlistint_t list
+ *
+ * @dlistint_t: list to be printed from
+ *@h: head pointer
+ *
+ * Return: number of node
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t node = 0;
+	while (h)
+	{
+		printf("%d\n", h->n );
+		++node;
+		h = h->next;
+	}
+
+	return (node);
+	
+}
