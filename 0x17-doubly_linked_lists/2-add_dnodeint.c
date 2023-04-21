bfeae1234d347ca361a207a27746c54104caf956
gerology <gerology007@gmail.com>
2023-04-21 15:14:45 +0000

    adds node at the start of list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds new node at the beginning of a dlistint_t list
+ *
+ * @head: head pointer
+ * @n: integer
+ *
+ * Return: new node, NULL if fail
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnd;
+
+	newnd = malloc(sizeof(dlistint_t));
+
+	if (newnd == NULL)
+	{
+		return (NULL);
+	}
+
+	newnd->n = n;
+	newnd->prev = NULL;
+	newnd->next = *head;
+
+	if (head == NULL)
+		(*head)->prev = newnd;
+	*head = newnd;
+
+	return (newnd);
+
+}
