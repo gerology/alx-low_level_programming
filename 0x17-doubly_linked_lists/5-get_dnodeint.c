0bc2b3477771850021262ed3fecabad64c1bb1f7
gerology <gerology007@gmail.com>
2023-04-22 10:51:11 +0000

    get the nth node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of linked list
+ *
+ * @head: head pointer
+ * @index: position of nth node
+ *
+ * Return: adress of node, or NULL if it doesnt exist
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+	}
+
+	return (head);
+
+}
