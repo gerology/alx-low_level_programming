6be75be838d90b0a67f7b1490d487127398f345c
gerology <gerology007@gmail.com>
2023-04-22 07:24:50 +0000

    return number of elements in a list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,7 +3,6 @@
 /**
  * dlistint_len - returns the number of elements in a linked dlistint_t list
  *
- * @dlistint_t: list to print from
  * @h: head pointer
  *
  * Return: number of elements, NULL if fail
