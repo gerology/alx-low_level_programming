4a4c1419457a3034ea878e1381f626cd6fc42e00
gerology <gerology007@gmail.com>
2023-04-25 10:20:14 +0000

    insert node at given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	if (tmp->next == NULL)
-		return (add_dnodeint_end(h, n));
+		return (NULL);
 
 	new->n = n;
 	new->prev = tmp;
