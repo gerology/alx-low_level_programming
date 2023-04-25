13a0f08083e2a776ae7ea45100b54bf4853ba9f6
gerology <gerology007@gmail.com>
2023-04-25 14:20:14 +0000

    delete node at a given position

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes node at given index
+ *
+ * @head: head pointer
+ * @index: position of node to delete
+ *
+ * Return: i if success, -1 if failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tp;
+	tp = *head;
+
+	for (; index != 1; index--)
+	{
+		if (tp == NULL)
+			return (-1);
+		tp = tp->next;
+	}
+
+	if (tp == *head)
+	{
+		*head = tp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	} else {
+
+		tp->prev->next = tp->next;
+
+		if (tp->next != NULL)
+			tp->next->prev = tp->prev;
+	}
+
+	free(tp);
+	return (1);
+}
