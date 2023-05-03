690c85f57f49f229b1b438e356fe2d546949b825
gerology <gerology007@gmail.com>
2023-05-03 21:29:05 +0000

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,5 @@ void __attribute__((constructor)) autoprint(void);
 void autoprint(void)
 {
 	printf("You're beat! and yet, \
-			you must allow,\nI bore my house upon my back!\n");
+		you must allow,\nI bore my house upon my back!\n");
 }
