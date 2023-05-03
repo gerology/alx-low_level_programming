a8915767a552f062c948281fd030bc7a221e9382
gerology <gerology007@gmail.com>
2023-05-03 11:44:11 +0000

    copy

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,5 +14,5 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
-
+int copy_file_to_file(char *ff, char *ft);
 #endif /*  _MAIN_H_ */
