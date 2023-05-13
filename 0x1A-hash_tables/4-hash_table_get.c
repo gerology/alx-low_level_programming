907aff2f9badcbaf049554d62990ba4b366d63a1
gerology <gerology007@gmail.com>
2023-05-13 19:17:18 +0000

    get element from hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,13 +14,13 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long index, i;
-	hash_node_t * nood;
+	hash_node_t *nood;
 
 	if (ht == NULL || *key == '\0' || key == NULL)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	
+
 	if (index >= ht->size)
 		return (NULL);
 
