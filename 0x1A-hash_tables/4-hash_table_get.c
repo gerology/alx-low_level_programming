b79ed3806ff8b1a10d53424910c7db81f2817381
gerology <gerology007@gmail.com>
2023-05-13 18:36:03 +0000

    get element from hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_get - retrieve a value associatd with key
+ *
+ * @ht: the hash table to retrieve from
+ * @key: key of the value to be retrieved
+ *
+ * Return: value associated with key, or NULL if failed
+ *
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long index, i;
+	char * value;
+	
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	i = index;
+
+	while (ht->array[i])
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+			value = ht->array[i]->value;
+
+		i++;
+	}
+
+	return (value);
+}
